aeb96c66d6cf1cda3a2056b2262f13113776ae1b
Nieves Mª Borrero Barea <nievesborrerobarea@gmail.com>
2018-11-21 15:45:15 +0100

    commands, click added (#1)
    * commands, click added
    
    * refactor actions to const

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import os
 
+import click
+
 from services import TrelloClientService, UserStoryService
 
+ACTIONS = ['sync', 'update']
 
-def main():
+@click.command()
+@click.option('--action', type=click.Choice(ACTIONS), help='action to execute')
+
+def main(action):
+    switcher = {
+        'sync': sync,
+        'update': update
+    }
+    func = switcher.get(action)
+    return func()
+
+def sync():
     token = os.environ.get('TRELLO_TOKEN')
     app_key = os.environ.get('TRELLO_APP_KEY')
     board = os.environ.get('TRELLO_BOARD')
@@ -11,6 +25,8 @@ def main():
     user_story_service = UserStoryService()
     cards = user_story_service.process_cards(client_service.get_cards())
 
+def update():
+    print('TO DO update')
 
 if __name__ == '__main__':
     main()
