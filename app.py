d9ac86033857c04e4f73b1c3c9475a2533f20c85
nieves <nieves@nievess-MacBook-Pro.local>
2018-11-23 12:42:06 +0100

    board and path options of click added

--- app.py
+++ app.py
@@ -4,32 +4,19 @@ import click
 
 from services import TrelloClientService, UserStoryParser
 
-ACTIONS = ['sync', 'update']
-
 @click.command()
-@click.option('--action', type=click.Choice(ACTIONS), help='action to execute')
+@click.option('--board', required=True, help='board of project')
+@click.option('--path', required=True, help='output path')
 
-def main(action):
-    switcher = {
-        'sync': sync,
-        'update': update
-    }
-    func = switcher.get(action)
-    return func()
-
-def sync():
+def main(board, path):
     token = os.environ.get('TRELLO_TOKEN')
     app_key = os.environ.get('TRELLO_APP_KEY')
-    board = os.environ.get('TRELLO_BOARD')
     client_service = TrelloClientService(token, app_key, board)
     parser = UserStoryParser()
     cards = parser.get_cards_as_user_stories(client_service.get_cards())
     # TODO Save data from the cards
     for card in cards:
         print(card)
-
-def update():
-    print('TO DO update')
 
 if __name__ == '__main__':
     main()
