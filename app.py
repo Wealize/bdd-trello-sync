ac8f7b0f96e8c789e6f6f9d3ad56355da5c1bc3d
Nieves Mª Borrero Barea <nievesborrerobarea@gmail.com>
2018-11-23 16:31:26 +0100

    dotenv added and (#5)

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import os
 
 import click
+from dotenv import load_dotenv
 
 from services import TrelloClientService, UserStoryParser
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '.')
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
 
 @click.command()
 @click.option('--board', required=True, help='board of project')
 @click.option('--path', required=True, help='output path')
 
 def main(board, path):
-    token = os.environ.get('TRELLO_TOKEN')
-    app_key = os.environ.get('TRELLO_APP_KEY')
+    token = os.getenv('TRELLO_TOKEN')
+    app_key = os.getenv('TRELLO_APP_KEY')
     client_service = TrelloClientService(token, app_key, board)
     parser = UserStoryParser()
     cards = parser.get_cards_as_user_stories(client_service.get_cards())
